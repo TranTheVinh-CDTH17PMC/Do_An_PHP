4f3cfdf605c4c35afe70aaae7b2b9194d2c6fd42
NguyenHuyenThoai-CDTH17PMC <56819507+NguyenHuyenThoai-CDTH17PMC@users.noreply.github.com>
2019-11-14 21:25:43 +0700

    Update ThemCauHoiSeeder.php

--- database/seeds/ThemCauHoiSeeder.php
+++ database/seeds/ThemCauHoiSeeder.php
@@ -14,7 +14,7 @@ class ThemCauHoiSeeder extends Seeder
         DB::table('cau_hoi')->insert([
         	['noi_dung'=>'Giải Grand Slam đầu tiên trong năm là giải nào?','linh_vuc_id'=>1,'phuong_an_a'=>'Austrlia mở rộng','phuong_an_b'=>'Wimbledon','phuong_an_c'=>'Roland Garos','phuong_an_d'=>'Mỹ mở rộng','dap_an'=>'A'],
             ['noi_dung'=>'Cùng với Hà Nội T&T (vô địch V-League 2010), CLB nào của Việt Nam tham dự AFC Cup 2011?','linh_vuc_id'=>1,'phuong_an_a'=>'Sông Lam Nghệ An','phuong_an_b'=>'SHB Đà Nẵng','phuong_an_c'=>'Hoàng Anh Gia Lai','phuong_an_d'=>'Becamex Bình Dương','dap_an'=>'A'],
-            ['noi_dung'=>'AFC Asian Cup 2011 được tổ chức tại quốc gia nào?','linh_vuc_id'=>1,'phuong_an_a'=>'Qatar','phuong_an_b'=>'Hàn Quốc','phuong_an_c'=>'Nhật Bản','phuong_an_d'=>'Ỉraq','dap_an'=>'A'],
+            ['noi_dung'=>'AFC Asian Cup 2011 được tổ chức tại quốc gia nào?','linh_vuc_id'=>1,'phuong_an_a'=>'Qatar','phuong_an_b'=>'Hàn Quốc','phuong_an_c'=>'Nhật Bản','phuong_an_d'=>'Iraq','dap_an'=>'A'],
             ['noi_dung'=>'Đội nào lên ngôi vô địch AFC Asian Cup 2011 tổ chức tại Qatar?','linh_vuc_id'=>1,'phuong_an_a'=>'Nhật Bản','phuong_an_b'=>'Austrlia','phuong_an_c'=>'Hàn Quốc','phuong_an_d'=>'Uzbekistan','dap_an'=>'A'],
             ['noi_dung'=>'Việt Nam lần đầu tiên vô địch AFF Cup là vào năm nào?','linh_vuc_id'=>1,'phuong_an_a'=>'2004','phuong_an_b'=>'2006','phuong_an_c'=>'2008','phuong_an_d'=>'2010','dap_an'=>'C'],
             ['noi_dung'=>'CLB nào vô địch FIFA Club World Cup 2010 tổ chức tại Qatar?','linh_vuc_id'=>1,'phuong_an_a'=>'Inter Milan','phuong_an_b'=>'Real Madrid','phuong_an_c'=>'Bayern Munich','phuong_an_d'=>'Manchester United','dap_an'=>'A'],
