2da06dfea177c79c50fde9da69c4ac5d28d8e1b4
TranTheVinh-CDTH17PMC <56820186+TranTheVinh-CDTH17PMC@users.noreply.github.com>
2020-01-02 22:09:11 +0700

    a

--- resources/views/ds_cauhinhapp.blade.php
+++ resources/views/ds_cauhinhapp.blade.php
@@ -8,13 +8,17 @@
                         <div class="card">
                             <div class="card-body">
                                 <h4 class="header-title">Danh sách cấu hình</h4>
+                                <a href="#"><button class="btn btn-primary waves-effect waves-light" type="button">Thêm mới</button></a>
+                                <a href="#"><button class="btn btn-primary waves-effect waves-light" type="button">Thùng rác</button></a>
+
                                 <table id="basic-datatable" class="table dt-responsive nowrap">
                                     <thead>
                                         <tr>
                                             <th>ID</th>
                                             <th>Cơ hội sai </th>
                                             <th>Thời gian trả lời (giây) </th>
-                                            <th>Cập Nhật</th>
+                                            <th>Sửa | Xóa</th>
+                                            
                                         </tr>
                                     </thead>
                                 
@@ -31,13 +35,21 @@
                                                      
                                                
                                             
-                                                    <a href="{{ route('ds_cauhinhapp.ds_cauhinhapp.cs-them-moi-cau-hinh-app',$cauhinhapp->id) }}">
+                                                    <a href="#">
                                                       <button class="btn btn-purple waves-effect waves-light" type="button"><i class=" mdi mdi-pencil-outline"></i></button></a> 
                                                     
                                                      <script language="javascript">
                                                     function check_register()
                                                     // kiem tra cac textbox
-                                                     </script>                                               
+                                                     </script>
+                                                    <form method="POST" action="#"  onsubmit="return check_register();">
+                                                        {{ method_field('DELETE') }}
+                                                        {{csrf_field()}}
+                                                       
+                                                       <button type="submit" id="submit_xoa" class="btn btn-danger waves-effect waves-light" onclick="return confirm('Bạn có chắclà muốn xóa dữ liệu ?');" ><i class="mdi mdi-trash-can-outline"></i>
+                                                        </button>
+                                                       </form>
+                                               
                                             </td>
                                            
                                         </tr>
